01815ac1fcb56ffca5f6ebc62cd79957cd5cdc57
hcigarroa <hcigarroa@gmail.com>
2022-06-30 14:59:52 -0500

    se agregp logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,16 @@
+import React from 'react';
+import imgLogo from "../assets/img/logo.png";
 function NavBar(){
     return(
     <nav>
-        <img src="../src/assets/img/PuCo.png" alt="Logo Puro Corazón"/>
+        <img width="120" src={imgLogo} alt="Logo Tienda"/>
         <h2>Puro Corazón</h2>
         <h3>Artesania Contemporánea</h3>   
         <ul>
             <li>
                 <a href="/">Inicio</a>
                 <a href="/">Nosotros</a>
-                <a href="/">productos</a>
+                <a href="/">Productos</a>
                 <a href="/">Contacto</a>
             </li>
         </ul>
