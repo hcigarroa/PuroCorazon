9ac31be5c034147aa9000159ef1ea689bdf59c9e
hcigarroa <hcigarroa@gmail.com>
2022-06-30 14:58:13 -0500

    Segrego logo

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+function NavBar(){
+    return(
+    <nav>
+        <img src="../src/assets/img/PuCo.png" alt="Logo Puro Corazón"/>
+        <h2>Puro Corazón</h2>
+        <h3>Artesania Contemporánea</h3>   
+        <ul>
+            <li>
+                <a href="/">Inicio</a>
+                <a href="/">Nosotros</a>
+                <a href="/">productos</a>
+                <a href="/">Contacto</a>
+            </li>
+        </ul>
+        
+    </nav>
+    )
+}
+
+
+
+export default NavBar;
