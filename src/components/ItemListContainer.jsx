76a8ed5d8c34e538730a22d3b78b89017c3f6607
hcigarroa <hcigarroa@gmail.com>
2022-07-08 18:25:46 -0500

    Mas Avance

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,13 @@
 import React from "react"
-function ItemListContainer(props) {
+function ItemListContainer({titulo}) {
     return (
-        <>
+        <div className="container px-5 py-8 mx-auto">
+            <div className="flex flex-col text-center w-ful mb-12">
+                <h1 className="sm:text-3x1 text-2x1 font-bold title-font mb-4 text-pink-600">{ titulo }</h1>
+            </div>
         <h1>{props.ItemListContainer}</h1>
         <div>ItemListContainer</div>
-        </>
+        </div>
     );
 }
 
