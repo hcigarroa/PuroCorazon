a220e9dcb642f8095524d3c7961843b8ce990ba6
hcigarroa <hcigarroa@gmail.com>
2022-07-22 18:11:28 -0500

    compilando 2

--- src/components/itemDetailContainer.js
+++ src/components/itemDetailContainer.js
@@ -1,22 +1,20 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ItemDetail from "./ItemDetail";
 import { useParams } from "react-router-dom";
 import detailledItems from "./utils/details.json"
 import Spinner from "./Spinner";
 
-
 const ItemDetailContainer = () => {
-  const [item, setItem] = useState(false);
+  const [item, setItem] = useState({});
   const [loading, setLoading] = useState(false);
   const params = useParams();
-  const Promise = new Promise((resolve, reject) =>{
-    setTimeout(() => resolve(detailledItems), 2000);
+  const promise = new Promise((res, rej) =>{
+    setTimeout(() => res(detailledItems), 2000);
   });
-
 
 useEffect(() => {
     setLoading(true);
-    Promise.then((response) => {
+    promise.then((response) => {
       const foundItem = response.filter((item) => item.id == params.id);
       setItem(foundItem[0]);
       setLoading(false);
