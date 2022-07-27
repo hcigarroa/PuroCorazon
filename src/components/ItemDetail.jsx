08ffc82ebb0d2fdaa1e6cb7e8095e6afb2595e63
hcigarroa <hcigarroa@gmail.com>
2022-07-26 21:13:20 -0500

    Sincronizar Counter

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,49 +1,36 @@
 import React, { useState } from "react";
-import {default as ItemCount} from "../components/ItemCount"
+import ItemCount from "./ItemCount";
 import { useNavigate, Link } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
-const ItemDetail  = ({ item }) => {
+const ItemDetail = ({ item }) => {
   const [amount, setAmount] = useState(0);
-  const {precio, stock, material, pictureUrl, id} = item;
+  const { id, precio, stock, pictureUrl, material} = item;
   const onAdd = (amount) => {
-  setAmount(amount);
+    const navigate = useNavigate();
+    setAmount(amount);
+    navigate("/cart");
   }; 
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    return (
+      <div className="card__item">
+        <img className="card-img-top" src={pictureUrl} alt="card image cap" />
+      <div className="card__item-descripion">
+        <h5 className="card-title">{id}</h5>
+        <p className="card-text">{material}</p>
+        <p className="card-text">{precio}</p>
+      </div>
+  
+      {amount == 0 ? (
+        <ItemCount stock={stock} initial={0} onAdd={onAdd} />
+      ) : (
+        <h1>{amount} Productos a comprar</h1>
+      )}
+      <div className="d-flex justify-content-center my-3">
+        <Link to="/cart/">
+          <button className="btn btn-warning">Go to checkout</button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
 export default ItemDetail;
-
-//  const ItemDetail = ({item}) => {
-//    const [amount, setAmount] = useState(0);
-//   const {precio, stock, material, pictureUrl, id} = item
-//    const onAdd = (amount);
-//  }; 
-//  return (
-//      <>
-//           <div className="card__item">
-//        <div className="card__item-descripion">
-//          <h1>{props.data.title}</h1>
-//          <img src={props.data.pictureUrl} alt="product img" />
-//          <ItemDescriptionContainer item={props.data} key={props.data.is} />
-//          <span>Price:${props.data.price} | in stock:{props.data.stock}</span>
-//        </div>
-//      </div>
-//      </>
-//    );
-//  };
-
-//  export default ItemDetail;
